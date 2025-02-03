41180e5ed46999cade7c37d2b59414efbafb3652
kwamevaughan <kwamevaughan@gmail.com>
2025-02-03 09:05:00 +0000

    UI improvement

--- src/components/AppDownloadModal.js
+++ src/components/AppDownloadModal.js
@@ -1,13 +1,27 @@
 // components/AooDownloadModal.js
 import React from 'react';
 import Modal from './Modal'; // Import the Modal component
+import Image from 'next/image';
 
 const AppDownloadModal = ({ isOpen, onClose }) => {
     return (
         <Modal isOpen={isOpen} onClose={onClose} title="Download Mobile App">
             <p>
-                App download. Testing
+                Download our mobile app to earn points.
             </p>
+            <div className="flex ">
+                <Image src="/assets/images/android.svg" alt="Android"
+                       width={75}
+                       height={75}
+                       className="transition-transform transform hover:translate-y-[-5px] duration-500 ease-in-out"
+                />
+
+                <Image src="/assets/images/apple.svg" alt="Apple"
+                       width={75}
+                       height={75}
+                       className="transition-transform transform hover:translate-y-[-5px] duration-500 ease-in-out"
+                />
+            </div>
         </Modal>
     );
 };
