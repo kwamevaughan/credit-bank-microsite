66355118b21b8a67b6698ef3bf49732cec730bbe
kwamevaughan <kwamevaughan@gmail.com>
2025-02-03 09:46:15 +0000

    UI improvement

--- src/components/AppDownloadModal.js
+++ src/components/AppDownloadModal.js
@@ -1,26 +1,46 @@
-// components/AooDownloadModal.js
 import React from 'react';
 import Modal from './Modal'; // Import the Modal component
 import Image from 'next/image';
+import Link from 'next/link'; // Import Link from nextjs
 
 const AppDownloadModal = ({ isOpen, onClose }) => {
     return (
         <Modal isOpen={isOpen} onClose={onClose} title="Download Mobile App">
-            <p>
-                Download our mobile app to earn points.
+            <p className="text-center mb-4">
+                Download the Credit Bank mobile app to earn points.
             </p>
-            <div className="flex ">
-                <Image src="/assets/images/android.svg" alt="Android"
-                       width={75}
-                       height={75}
-                       className="transition-transform transform hover:translate-y-[-5px] duration-500 ease-in-out"
-                />
+            <div className="flex justify-center gap-x-14">
+                <div>
+                    <Link
+                        href="https://play.google.com/store/apps/details?id=co.ke.ekenya.creditbank"
+                        passHref
+                    >
+                        <Image
+                            src="/assets/images/android.svg"
+                            alt="Android"
+                            width={75}
+                            height={75}
+                            className="transition-transform transform hover:translate-y-[-5px] duration-500 ease-in-out"
+                        />
+                        <span>Android users</span>
+                    </Link>
+                </div>
 
-                <Image src="/assets/images/apple.svg" alt="Apple"
-                       width={75}
-                       height={75}
-                       className="transition-transform transform hover:translate-y-[-5px] duration-500 ease-in-out"
-                />
+                <div>
+                    <Link
+                        href="https://apps.apple.com/us/app/credit-bank-cb-konnect/id1469515952"
+                        passHref
+                    >
+                        <Image
+                            src="/assets/images/apple.svg"
+                            alt="Apple"
+                            width={75}
+                            height={75}
+                            className="transition-transform transform hover:translate-y-[-5px] duration-500 ease-in-out"
+                        />
+                        <span>Apple users</span>
+                    </Link>
+                </div>
             </div>
         </Modal>
     );
