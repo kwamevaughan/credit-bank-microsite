105eb8fa4a68854003da619994a2c6466e0123d0
kwamevaughan <kwamevaughan@gmail.com>
2025-02-10 05:53:10 +0000

    Dark mode support

--- src/components/DeleteAccountModal.js
+++ src/components/DeleteAccountModal.js
@@ -1,26 +1,39 @@
-import Modal from './Modal'; // Import the generic Modal component
+import Modal from './Modal';
 
-const DeleteAccountModal = ({ isOpen, onClose, handleDeleteAccount }) => {
+const DeleteAccountModal = ({ handleDeleteAccount, isOpen, onClose, mode }) => {
+
     return (
-        <Modal
-            isOpen={isOpen}
-            onClose={onClose}
-            title="Delete Account"
-        >
-            <p className=" mb-4">
-                This action is irreversible. Your account will be permanently deleted.
-            </p>
-            <div className="flex justify-between">
-                <button
-                    onClick={onClose}
-                    className="bg-gray-300 text-black px-4 py-2 rounded-lg hover:bg-gray-400 transition-all duration-300 ease-in-out">
-                    Cancel
-                </button>
-                <button
-                    onClick={handleDeleteAccount}
-                    className="bg-red-600 text-white px-4 py-2 rounded-lg hover:bg-red-700 transition-all duration-300 ease-in-out">
-                    Delete Account
-                </button>
+        <Modal isOpen={isOpen} onClose={onClose}>
+            <div
+                className={`p-6 ${
+                    mode === 'dark' ? 'bg-[#0f1720] text-white' : 'bg-white text-black'
+                } p-4 rounded-md text-center`}
+            >
+                <div>
+                    <h2
+                        className={`${
+                            mode === 'dark' ? 'text-white' : 'text-black'
+                        } text-xl font-semibold mb-4`}
+                    >
+                        Delete Account?
+                    </h2>
+                </div>
+
+                <p className="mb-4">
+                    This action is irreversible. Your account will be permanently deleted.
+                </p>
+                <div className="flex justify-between">
+                    <button
+                        onClick={onClose}
+                        className={`px-4 py-2 rounded-lg transition-all duration-300 ease-in-out ${mode === 'dark' ? 'bg-gray-200 text-black hover:bg-gray-500' : 'bg-gray-300 text-black hover:bg-gray-400'}`}>
+                        Cancel
+                    </button>
+                    <button
+                        onClick={handleDeleteAccount}
+                        className={`px-4 py-2 rounded-lg transition-all duration-300 ease-in-out ${mode === 'dark' ? 'bg-red-600 text-white hover:bg-red-700' : 'bg-red-600 text-white hover:bg-red-700'}`}>
+                        Delete Account
+                    </button>
+                </div>
             </div>
         </Modal>
     );
