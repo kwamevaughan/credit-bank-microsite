10666796e246ea593239b19cd4f7eaf18de890d6
kwamevaughan <kwamevaughan@gmail.com>
2025-02-12 12:42:33 +0000

    installed Google Analytics

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,5 +1,3 @@
-// pages/_document.js
-
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
@@ -18,6 +16,18 @@ class MyDocument extends Document {
                     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
                     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
                     <title>Credit Bank Diaspora Campaign</title>
+                    {/* Google Analytics Script */}
+                    <script async src="https://www.googletagmanager.com/gtag/js?id=G-54291TSTMQ"></script>
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                                window.dataLayer = window.dataLayer || [];
+                                function gtag(){dataLayer.push(arguments);}
+                                gtag('js', new Date());
+                                gtag('config', 'G-54291TSTMQ');
+                            `,
+                        }}
+                    />
                 </Head>
                 <body>
                 <Main />
