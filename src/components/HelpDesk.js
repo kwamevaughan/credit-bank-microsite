ffe2fb92d827987d0982afa0c14112a072f2cea6
kwamevaughan <kwamevaughan@gmail.com>
2025-02-18 20:54:48 +0000

    new comp

---
+++ src/components/HelpDesk.js
@@ -0,0 +1,12 @@
+// HelpDesk.js
+import React from 'react';
+
+const HelpDesk = () => {
+    return (
+        <p className="text-sm text-gray-500 dark:text-gray-400">
+            Temporarily unavailable. Please check back later.
+        </p>
+    );
+};
+
+export default HelpDesk;
