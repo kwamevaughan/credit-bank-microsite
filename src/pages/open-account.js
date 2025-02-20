629e7aecd301e2b96c81458ffe51bc781f72d8de
kwamevaughan <kwamevaughan@gmail.com>
2025-02-20 21:26:55 +0000

    improved UI

--- src/pages/open-account.js
+++ src/pages/open-account.js
@@ -11,7 +11,7 @@ import useTheme from '@/hooks/useTheme';
 import useSidebar from '@/hooks/useSidebar';
 import useSignOut from '@/hooks/useSignOut';
 import useAccountOpening from '@/hooks/useAccountOpening';
-import { CheckIcon, XMarkIcon } from '@heroicons/react/24/outline';
+import { ShieldCheckIcon } from '@heroicons/react/24/outline';
 
 const OpenAccount = () => {
     const router = useRouter();
@@ -113,7 +113,61 @@ const OpenAccount = () => {
                                     the Nyumbani Diaspora Challenge, you can earn points for opening an account or
                                     referring a friend to open one.
                                 </p>
+
+                                <div className="flex flex-col md:flex-row items-stretch justify-between gap-6">
+                                    <div
+                                        className="flex flex-col border border-[#ff930a] p-4 rounded-lg hover:shadow-lg hover:translate-y-[-8px] transform transition-all duration-300 ease-in-out w-full md:w-[48%] flex-grow">
+                                        <div className="flex items-center mb-2">
+                                            <ShieldCheckIcon className="w-6 h-6 text-teal-500 mr-2"/>
+                                            <p className={`font-extrabold text-base ${mode === 'dark' ? 'text-gray-300' : 'text-gray-700'}`}>
+                                                CDSC Account
+                                            </p>
+                                        </div>
+                                        <span
+                                            className={`text-base pb-4 ${mode === 'dark' ? 'text-gray-300' : 'text-gray-700'}`}>
+                Open a CDSC account if you have a Nyumbani Diaspora Account and wish to buy and sell listed stocks at the Nairobi Securities Exchange.
+            </span>
+
+                                        <div className="flex-grow"></div>
+
+                                        <a href="https://creditbank.co.ke/nse-cdsc-accounts/" target="_blank"
+                                           rel="noopener noreferrer">
+                                            <button
+                                                className="bg-[#0cb4ab] hover:bg-gray-400 text-white font-bold py-2 px-4 rounded-lg">
+                                                Open a CDSC Account
+                                            </button>
+                                        </a>
+                                    </div>
+
+                                    <div
+                                        className="flex flex-col border border-[#ff930a] p-4 rounded-lg hover:shadow-lg hover:translate-y-[-8px] transform transition-all duration-300 ease-in-out w-full md:w-[48%] flex-grow">
+                                        <div className="flex items-center mb-2">
+                                            <ShieldCheckIcon className="w-6 h-6 text-teal-500 mr-2"/>
+                                            <p className={`font-extrabold text-base ${mode === 'dark' ? 'text-gray-300' : 'text-gray-700'}`}>
+                                                Fixed Deposit Account
+                                            </p>
+                                        </div>
+                                        <span
+                                            className={`text-base pb-4 ${mode === 'dark' ? 'text-gray-300' : 'text-gray-700'}`}>
+                This investment account gives you the opportunity to grow your wealth and meet your financial goals in an assured manner. You can enjoy a competent fixed interest rate on your deposits until a given maturity date.
+            </span>
+
+                                        <div className="flex-grow"></div>
+
+                                        <a href="https://creditbank.co.ke/fixed-call-deposit-account/" target="_blank"
+                                           rel="noopener noreferrer">
+                                            <button
+                                                className="bg-[#0cb4ab] hover:bg-gray-400 text-white font-bold py-2 px-4 rounded-lg">
+                                                Open a Fixed Account
+                                            </button>
+                                        </a>
+                                    </div>
+                                </div>
+
+                                <p className="pt-4 text-lg font-bold">Already opened an account? Fill in your details to redeem your
+                                    points.</p>
                             </div>
+
 
                             <div className="flex flex-col justify-center px-4 space-y-4">
                                 {/* Form with 3/4 width */}
@@ -210,17 +264,19 @@ const OpenAccount = () => {
                                     </div>
 
                                     {/* Submit Button */}
-                                    <div className="flex pt-4 gap-6 w-full justify-between items-center">
-                                        <span className="flex-grow w-3/4">
-                                            Your points will be updated after our 24-hour verification process.
-                                        </span>
+                                    <div
+                                        className="flex flex-col md:flex-row pt-4 gap-6 w-full justify-between items-center">
+    <span className="flex-grow w-full md:w-3/4">
+        Your points will be updated after our 24-hour verification process.
+    </span>
                                         <button
                                             type="submit"
-                                            className={`w-1/2 py-4 px-4 rounded-lg font-bold focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50 ${mode === 'dark' ? 'bg-teal-600 text-white hover:bg-teal-700' : 'bg-teal-500 text-white hover:bg-teal-600'}`}
+                                            className={`w-full md:w-1/2 py-4 px-4 rounded-lg font-bold focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50 ${mode === 'dark' ? 'bg-teal-600 text-white hover:bg-teal-700' : 'bg-teal-500 text-white hover:bg-teal-600'}`}
                                         >
                                             Redeem Points
                                         </button>
                                     </div>
+
                                 </form>
                             </div>
                         </div>
