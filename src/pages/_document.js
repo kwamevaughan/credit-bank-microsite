d707549a32b7cdac92da317d90161171f26e6c4c
kwamevaughan <kwamevaughan@gmail.com>
2025-02-21 14:12:48 +0000

    Installed Google Tag Manager

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -16,20 +16,33 @@ class MyDocument extends Document {
                     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
                     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
                     <title>Credit Bank Diaspora Campaign</title>
-                    {/* Google Analytics Script */}
-                    <script async src="https://www.googletagmanager.com/gtag/js?id=G-54291TSTMQ"></script>
+                    {/* Google Tag Manager Script */}
                     <script
                         dangerouslySetInnerHTML={{
                             __html: `
-                                window.dataLayer = window.dataLayer || [];
-                                function gtag(){dataLayer.push(arguments);}
-                                gtag('js', new Date());
-                                gtag('config', 'G-54291TSTMQ');
+                                (function(w,d,s,l,i){
+                                    w[l]=w[l]||[]; 
+                                    w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'}); 
+                                    var f=d.getElementsByTagName(s)[0], 
+                                    j=d.createElement(s), dl=l!='dataLayer'?'&l='+l:''; 
+                                    j.async=true; 
+                                    j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl; 
+                                    f.parentNode.insertBefore(j,f);
+                                })(window,document,'script','dataLayer','GTM-KTJTPV6R');
                             `,
                         }}
                     />
                 </Head>
                 <body>
+                {/* Google Tag Manager (noscript) */}
+                <noscript>
+                    <iframe
+                        src="https://www.googletagmanager.com/ns.html?id=GTM-KTJTPV6R"
+                        height="0"
+                        width="0"
+                        style={{ display: 'none', visibility: 'hidden' }}
+                    ></iframe>
+                </noscript>
                 <Main />
                 <NextScript />
                 </body>
