16bf81aef332845ccc6e31e483ee87079c2d38f8
kwamevaughan <kwamevaughan@gmail.com>
2025-02-21 14:39:13 +0000

    added tracking ids

--- src/layouts/nav-header.js
+++ src/layouts/nav-header.js
@@ -90,6 +90,7 @@ export default function NavHeader() {
                 <div className="hidden md:flex sm:ml-auto space-x-4">
                     <Link href="/participate">
                         <button
+                            id="sign-up-button"
                             className="px-6 py-2 text-md rounded-lg font-bold text-white border-2 border-teal-400 bg-teal-400 transition-all ease-in-out duration-300 hover:bg-transparent hover:text-teal-400">
                         Join Now
                         </button>
