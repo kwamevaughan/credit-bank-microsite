d07b6bf1cd2f0e89309a9744da082d2e5c535b9a
kwamevaughan <kwamevaughan@gmail.com>
2025-02-22 03:08:00 +0000

    renamed page title

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -15,7 +15,7 @@ class MyDocument extends Document {
                     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
                     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
                     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-                    <title>Credit Bank Diaspora Campaign</title>
+                    <title>Credit Bank Diaspora Champions Challenge</title>
                     {/* Google Tag Manager Script */}
                     <script
                         dangerouslySetInnerHTML={{
