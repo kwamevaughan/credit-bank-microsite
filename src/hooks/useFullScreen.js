45ddd2e74c1f8da26c0815f8bdb7b9d5c67c4f71
kwamevaughan <kwamevaughan@gmail.com>
2025-02-23 07:09:02 +0000

    added mobile support

--- src/hooks/useFullScreen.js
+++ src/hooks/useFullScreen.js
@@ -2,13 +2,25 @@ import { useCallback } from 'react';
 
 const useFullScreen = () => {
     const toggleFullScreen = useCallback(() => {
-        if (!document.fullscreenElement) {
-            document.documentElement.requestFullscreen().catch(err => {
-                console.error(`Error attempting to enable full-screen mode: ${err.message}`);
-            });
+        if (!document.fullscreenElement && !document.webkitFullscreenElement) { // Check for fullscreen on both desktop and mobile
+            if (document.documentElement.requestFullscreen) {
+                document.documentElement.requestFullscreen(); // Standard Fullscreen API
+            } else if (document.documentElement.webkitRequestFullscreen) {
+                document.documentElement.webkitRequestFullscreen(); // Safari / iOS Fullscreen API
+            } else if (document.documentElement.mozRequestFullScreen) {
+                document.documentElement.mozRequestFullScreen(); // Firefox Fullscreen API
+            } else if (document.documentElement.msRequestFullscreen) {
+                document.documentElement.msRequestFullscreen(); // IE/Edge Fullscreen API
+            } else {
+                console.error("Fullscreen API not supported.");
+            }
         } else {
             if (document.exitFullscreen) {
-                document.exitFullscreen();
+                document.exitFullscreen(); // Exit fullscreen for desktop
+            } else if (document.webkitExitFullscreen) {
+                document.webkitExitFullscreen(); // Exit fullscreen for Safari/iOS
+            } else {
+                console.error("Exit fullscreen not supported.");
             }
         }
     }, []);
